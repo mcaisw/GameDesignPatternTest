2789c0e318afb9a9ad959dc125be263a024673c2
MengChi <mc2311362372@gmail.com>
2017-08-01 14:14:24 +0800

    角色接口

---
+++ Assets/MCScript/ICharacter.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class ICharacter {
+
+    protected GameObject mc_gameobject = null;//unity模型
+    //角色接口
+    public ICharacter() { }
+}
