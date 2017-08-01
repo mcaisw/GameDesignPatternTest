bdf359e2dae17fcd9f9c1a470b38c5d1086ae688
MengChi <mc2311362372@gmail.com>
2017-08-01 18:56:04 +0800

    ..

--- Assets/MCScript/IAttrStrategy.cs
+++ Assets/MCScript/IAttrStrategy.cs
@@ -1,11 +1,25 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 /*角色属性计算类，之所以把它声明成抽象类，是因为士兵和敌人都有各自的计算方案*/
-public abstract class IAttrStrategy {
-
+public abstract class IAttrStrategy
+{
+    public abstract void InitAttr(ICharacterAttr CharacterAttr);//初始化角色属性
 	
 }
-public class SoldierAttrStrategy : IAttrStrategy { }
+public class SoldierAttrStrategy : IAttrStrategy
+{
+    public override void InitAttr(ICharacterAttr CharacterAttr)
+    {
+        throw new NotImplementedException();
+    }
+}
 
-public class EnemyAttrStrategy : IAttrStrategy { }
+public class EnemyAttrStrategy : IAttrStrategy
+{
+    public override void InitAttr(ICharacterAttr CharacterAttr)
+    {
+        throw new NotImplementedException();
+    }
+}
--- Assets/MCScript/ICharacterAttr.cs
+++ Assets/MCScript/ICharacterAttr.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 /*角色属性类*/
 public abstract class ICharacterAttr{
 
+    protected IAttrStrategy mc_AttrStrategy = null;//引用
 
     public ICharacterAttr() { }
 
@@ -20,12 +21,22 @@ public abstract class ICharacterAttr{
         get { return mc_MoveSpeed; }
     }
     #endregion
+
+    //设置计算策略
+    public void SetAttrStrategy(IAttrStrategy AttrStrategy) {
+        mc_AttrStrategy = AttrStrategy;
+    }
+
 }
 
 public class SoldierAttr: ICharacterAttr {
 
+    protected int mc_soldierLV = 0;//士兵等级
+    protected int mc_AddMaxHP;//士兵新增的HP值
+
 }
 
-public class EnemyAttr : ICharacterAttr {
-
+public class EnemyAttr : ICharacterAttr
+{
+    protected int mc_CritRate = 0;//暴击概率
 }
