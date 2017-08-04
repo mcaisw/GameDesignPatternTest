3900780cf7e57a511030a9f33a919399044f78fc
MengChi <mc2311362372@gmail.com>
2017-08-04 14:45:32 +0800

    ..

---
+++ Assets/MCScript/ICharacterFactory.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class ICharacterFactory
+{
+    //产生soldier
+
+    //产生enemy
+}
+
+public class ConcreteCharacterFactory : ICharacterFactory
+{//整合原来的SoldierCamp产生士兵,StageSystem产生敌人
+
+}
