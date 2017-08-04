f20709503f0ef993aa6d7f615d677f0f2f3f7222
MengChi <mc2311362372@gmail.com>
2017-08-04 14:59:38 +0800

    ..

--- Assets/MCScript/ICharacterFactory.cs
+++ Assets/MCScript/ICharacterFactory.cs
@@ -1,15 +1,30 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public abstract class ICharacterFactory
 {
     //产生soldier
-
+    public abstract ISoldier CreatSoldier();
     //产生enemy
+    public abstract IEnemy CreatEnemy();
 }
 
 public class ConcreteCharacterFactory : ICharacterFactory
 {//整合原来的SoldierCamp产生士兵,StageSystem产生敌人
+    public override IEnemy CreatEnemy()
+    {
 
+        IEnemy theEnemy = null;
+
+        return theEnemy;
+    }
+
+    public override ISoldier CreatSoldier()
+    {
+        ISoldier theSoldier = null;
+
+        return theSoldier;
+    }
 }
