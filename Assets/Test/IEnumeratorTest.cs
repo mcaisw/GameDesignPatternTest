ff7cf407cd302e9e801eb4421e02d42c361fad80
MengChi <mc2311362372@gmail.com>
2017-08-08 10:51:29 +0800

    ..

---
+++ Assets/Test/IEnumeratorTest.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//IEnumerator的练习
+
+public class IEnumeratorTest : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
