cd124eefdef0e353e48f08a939211749fee9dd1b
MengChi <mc2311362372@gmail.com>
2017-08-10 10:18:59 +0800

    ··

--- Assets/Test/ReferenceTest/gameloop.cs
+++ Assets/Test/ReferenceTest/gameloop.cs
@@ -6,14 +6,14 @@ public  class gameloop : MonoBehaviour
 {
     GameObject theCube = null;
     public static gameloop instance;
-    //CubeCreator cubeCreat = new CubeCreator();
-  
+    //CubeCreator cubeCreat = new CubeCreator(); 继承MonoBehaviour不能用new，
+
     Test test = new Test();
 
     // Use this for initialization
     void Start ()
     {
-        CubeCreator cubeCreat = this.gameObject.AddComponent<CubeCreator>();
+        CubeCreator cubeCreat = this.gameObject.AddComponent<CubeCreator>();//用AddComponent代替new
         instance = this;
         theCube= cubeCreat.CreatCube("Enemy3");
         test.GetTheCube(theCube);
