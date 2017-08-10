8f16707f2b915dab80bd64763eb270fc68fdff40
MengChi <mc2311362372@gmail.com>
2017-08-10 10:46:06 +0800

    ··

---
+++ Assets/MCScript/Character/BaseAttr.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseAttr
+{
+    protected float mc_MaxHP = 0;//最大生命值（这个数值基本是不变的）
+    protected float mc_MoveSpeed = 0;//移动速度（也是基本不变的）    
+    public float Health
+    {
+        get { return mc_MaxHP; }
+    }
+
+    public float MoveSpeed
+    {
+        get { return mc_MoveSpeed; }
+    }
+}
