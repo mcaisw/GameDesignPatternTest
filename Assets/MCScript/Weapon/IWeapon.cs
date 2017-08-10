800714a3a78adb595977fc0fe547a5c926712be7
MengChi <mc2311362372@gmail.com>
2017-08-10 19:17:51 +0800

    ..

--- Assets/MCScript/Weapon/IWeapon.cs
+++ Assets/MCScript/Weapon/IWeapon.cs
@@ -10,12 +10,34 @@ public abstract class IWeapon{
 
     //引用
     protected ICharacter m_WeaponOwner = null;
+    protected WeaponAttr mc_WeaponAttr = null;
 
+    //额外增加的攻击力
+    protected int mc_AtkPlusValue = 0;
+
+    //获取攻击力
+    public int GetAtkValue {
+        get { return mc_WeaponAttr.GetAtkValue; }
+    }
+    //获取攻击距离
+    public float GetAtkRange {
+        get { return mc_WeaponAttr.GetAtkRange; }
+    }
     public GameObject Weapon_Model
     {
         get { return mc_WeaponGameObject; }
     }
-   
+    //设置攻击能力
+    public void SetWeaponAttr(WeaponAttr weaponattr)
+    {
+        mc_WeaponAttr = weaponattr;
+    }
+    //设置额外攻击力
+    public void SetAtkPlusValue(int value)
+    {
+        mc_AtkPlusValue = value;
+    }
+
     //功能
     public abstract void SetOwner(ICharacter Owner);//设置武器的拥有者
 
