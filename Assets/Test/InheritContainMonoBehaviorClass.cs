f391c5838bd404dfc9a5472fc70a45ab30ff3a61
MengChi <mc2311362372@gmail.com>
2017-08-17 17:51:28 +0800

    测试一个不继承MonoBehavior的类
    测试一个不继承MonoBehavior的类，但是继承了一个继承了MonoBehavior的父类，这个子类在调用父类的方法时，父类的start,update 方法均在被调用。

---
+++ Assets/Test/InheritContainMonoBehaviorClass.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InheritContainMonoBehaviorClass : InheritMonoBehaviorParentClass
+{
+
+    private void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        TestFunc();
+    }
+}
+/*这个类并没有继承MonoBehavior，但是它的父类继承了MonoBehavior
+ TestFunc()是它的父类的方法，在这里被调用，打印信息显示，它的父类的Start函数和Update函数都被调用了，
+ 说明它的父类被实例化出来一块儿内存，但是这块儿内存没有一个显示的指针。*/
---
+++ Assets/Test/InheritMonoBehaviorParentClass.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InheritMonoBehaviorParentClass : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Debug.Log("进入InheritMonoBehaviorParentClass的Start");
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Debug.Log("进入InheritMonoBehaviorParentClass的Update");
+	}
+
+    protected void TestFunc() {
+
+        Debug.Log("this is TestFunc");
+    }
+}
